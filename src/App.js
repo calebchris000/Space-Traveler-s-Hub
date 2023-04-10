7b57bc9efdff3fea4d6c8ca87456d62efb1bb355
calebchris000 <calebchris000@gmail.com>
2023-04-10 22:17:37 +0100

    Add skeleton for Rocket page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Profile from './components/Profile';
 import NavHeader from './components/NavHeader';
+import RocketPage from './components/Rocket';
 
 const App = () => (
   <>
@@ -9,6 +10,7 @@ const App = () => (
       <NavHeader />
       <Routes>
         <Route path="/Profile" element={<Profile />} />
+        <Route path="/Rocket" element={<RocketPage imgURL="https://i.imgur.com/DaCfMsj.jpg" />} />
       </Routes>
     </BrowserRouter>
   </>
