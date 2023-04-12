b33e1a88db60db10e0d0a8d2369db68e9a6f4a5b
calebchris000 <calebchris000@gmail.com>
2023-04-12 08:01:09 +0100

    Update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import RocketSlice from './rockets/RocketSlice';
+import MissionSlice from './missions/MissionSlice';
 
 const store = configureStore({
   reducer: {
     rocket: RocketSlice,
+    mission: MissionSlice,
   },
 });
 export default store;
