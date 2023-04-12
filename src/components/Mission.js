a9b42e59ab66c7b3dd5a2da0a2c6a3e0334d67df
calebchris000 <calebchris000@gmail.com>
2023-04-12 08:08:28 +0100

    Fix extension

--- src/components/Mission.js
+++ src/components/Mission.js
@@ -1,51 +1,92 @@
 import '../styles/Mission.css';
 import PropTypes from 'prop-types';
-import mission from './missionData';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect, useRef } from 'react';
+import { missionButtonReducer } from '../redux/missions/MissionSlice';
+import getMission from '../redux/missions/MissionAPI';
 
 const Mission = ({
+  id,
   mission,
   description,
-  status,
-  buttonStatus,
-}) => (
-  <section className="missionWrapper">
-    <div className="data-row">
-      <h3>{mission}</h3>
-      <p>{description}</p>
-      <p className="member">{status}</p>
-      <button type="button">
-        {buttonStatus}
-      </button>
-    </div>
-  </section>
-);
+}) => {
+  const member = useRef(null);
+  const dispatch = useDispatch();
+  const [missionText, setMissionText] = useState(
+    'Join Mission',
+  );
+  const [status, setStatus] = useState('NOT A MEMBER');
 
-const MissionPage = () => (
-  <>
-    <div className="titleRow">
-      <h3>Mission</h3>
-      <h3>Description</h3>
-      <h3>Status</h3>
-      <h3>.</h3>
-    </div>
+  function setMissionButton(e) {
+    if (missionText === 'Join Mission') {
+      setMissionText('Leave Mission');
+      setStatus('ACTIVE MEMBER');
+      member.current.classList.add('activeMember');
+      dispatch(
+        missionButtonReducer({ id, missionText, status }),
+      );
+      e.target.classList.add('leaveMission');
+    } else {
+      setMissionText('Join Mission');
+      setStatus('NOT A MEMBER');
+      member.current.classList.remove('activeMember');
+      e.target.classList.remove('leaveMission');
+      dispatch(
+        missionButtonReducer({ id, missionText, status }),
+      );
+    }
+  }
 
-    {mission.map((data) => (
-      <Mission
-        key={data.mission_id}
-        mission={data.mission_name}
-        description={data.description}
-        status="NOT A MEMBER"
-        buttonStatus="Join Mission"
-      />
-    ))}
-  </>
-);
+  return (
+    <section className="missionWrapper">
+      <div className="data-row">
+        <h3>{mission}</h3>
+        <p>{description}</p>
+        <p ref={member} className="member">{status}</p>
+        <button
+          className="missionButton"
+          onClick={(e) => setMissionButton(e)}
+          type="button"
+        >
+          {missionText}
+        </button>
+      </div>
+    </section>
+  );
+};
+
+const MissionPage = () => {
+  const dispatch = useDispatch();
+  const { content } = useSelector((store) => store.mission);
+  useEffect(() => {
+    dispatch(getMission());
+  }, [dispatch]);
+  return (
+    <>
+      <div className="titleRow">
+        <h3>Mission</h3>
+        <h3>Description</h3>
+        <h3>Status</h3>
+        <h3>.</h3>
+      </div>
+
+      {content.map((data) => (
+        <Mission
+          key={data.mission_id}
+          id={data.mission_id}
+          mission={data.mission_name}
+          description={data.description}
+          buttonStatus="Join Mission"
+        />
+      ))}
+    </>
+  );
+};
 
 Mission.propTypes = {
+  id: PropTypes.string.isRequired,
   mission: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
-  buttonStatus: PropTypes.string.isRequired,
 };
 
 export default MissionPage;
