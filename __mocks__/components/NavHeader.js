a162f7cafe6703f6656fb94665e3a942089c9028
Gmanlove <emmagman3@gmail.com>
2023-04-14 10:30:25 +0100

    Fix linter error

--- __mocks__/components/NavHeader.js
+++ __mocks__/components/NavHeader.js
@@ -1,20 +1,46 @@
-import { Link } from 'react-router-dom';
 import '../styles/NavHeader.css';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-const NavHeader = () => (
-  <header>
-    <div className="header_logo">
-      <img className="logo" src={logo} alt="logo" />
-      <h1>Space Traveler&apos;s Hub</h1>
-    </div>
-    <nav>
-      <li key={1}><NavLink to="/">Rockets</NavLink></li>
-      <li key={2}><NavLink to="/dragons">Dragons</NavLink></li>
-      <li key={3}><NavLink to="/Mission">Mission</NavLink></li>
-      <li key={4}><NavLink to="/My-Profile">My Profile</NavLink></li>
+const NavHeader = () => {
+  const urls = [
+    {
+      id: 1,
+      url: '/rocket',
+      text: 'Rocket',
+    },
+    {
+      id: 2,
+      url: '/mission',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      url: '/Profile',
+      text: 'My Profile',
+    },
+  ];
+
+  return (
+    <nav className="navbar">
+      <img src="3212580.png" className="PlantImg" alt="Planet-img" />
+      <h1 className="nav-heading">Space Travelers Hub</h1>
+
+      <ul className="nav-main">
+        {urls.map((url) => (
+          <li className="nav-items" key={url.id}>
+            <NavLink
+              to={url.url}
+              activeClassName="active"
+              exact
+            >
+              {url.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </nav>
-  </header>
-);
+  );
+};
 
 export default NavHeader;
